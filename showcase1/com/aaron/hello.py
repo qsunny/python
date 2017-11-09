698b3744532b0f80fec82f4a91eeb8b73bea1124
aaron <247986063@qq.com>
2017-11-09 19:10:13 +0800

    add hello.py

---
+++ showcase1/com/aaron/hello.py
@@ -0,0 +1,27 @@
+#-*- coding: <UTF-8> -*-
+
+def sayHello():
+    str = "hello world python!!!"
+    print(str);
+
+
+if __name__=="__main__"  :
+    # execute only if run as a script
+    print('This is main of module "hello.py"')
+    sayHello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
