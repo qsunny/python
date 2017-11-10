494e316a60f41bf5cfb7018fb56127e90210d146
aaron <247986063@qq.com>
2017-11-10 16:50:46 +0800

    add sys module test fun

--- showcase1/com/aaron/sysexample.py
+++ showcase1/com/aaron/sysexample.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from sys import argv
+#from sys import argv,path,modules,exec_prefix
+from sys import *
 
 def printCommandParam():
     """打印命令行参数"""
@@ -7,10 +8,39 @@ def printCommandParam():
     for commandParam in argv:
         print(commandParam)
 
+def printModuleSearchPath():
+    """打印模块搜索路径"""
+    print(type(path))
+    for subpath in path:
+        print(subpath)
+
+def printModuleDictionary():
+    """打印模块dictionary"""
+    for module in modules:
+        print(module)
+
+def printStaticObjectInfo():
+    """sys模块静态对象信息"""
+    #print(copyright)
+    print(exec_prefix)
+    print(executable)
+    print(hash_info)
+    print(implementation)
+    print(platform)
+    print(prefix)
+    print(thread_info)
+    print(version)
+    print(version_info)
+    # exit(2)
+    displayhook('a')
 
 
 if __name__=="__main__" :
     printCommandParam()
+    printModuleSearchPath()
+    printModuleDictionary()
+    #print(printCommandParam.__doc__)
+    printStaticObjectInfo()
 
 
 
