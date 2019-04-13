beb676fb8b4a095c14acb825e8f4e8113cb83cd2
aaron.qiu <247986063@qq.com>
2019-04-13 12:28:08 +0800

    flask_script manage custom command test

--- flask/microblog-webform/manage.py
+++ flask/microblog-webform/manage.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 from app import create_app
-from flask_script import Manager, Shell,Server
+from flask_script import Manager, Shell,Server,Command
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app.debug = True
@@ -23,6 +23,23 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context = make_shell_context))
 manager.add_command("runserver" ,Server(use_debugger=True, host='0.0.0.0', port=5000))
 
+'''自定义命令方法1'''
+class Hello(Command):
+    a = 1+1;
+    print('hello world %s'%a)
+
+    def run(self):
+        print("hello world1111")
+
+@manager.command
+def hello_world():
+    print("通过修饰符自定义命令Test")
+
+@manager.option("-n","--name",dest='name',default="world")
+def hello_world2(name):
+    print("通过修饰符自定义命令Test. "+"hello,"+name)
+
+manager.add_command('hello',Hello())
 
 if __name__ == '__main__':
     manager.run()
