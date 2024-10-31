53fa2ff86586ea920d62af4ade21dfd81402a522
aaron.qiu <247986063@qq.com>
2024-10-31 16:32:01 +0800

    feat:飞书机器人消息测试

---
+++ ssl/feishu.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+
+def send_feishu_msg(content=None):
+    try:
+        # 目标URL
+        url = 'https://open.feishu.cn/open-apis/bot/v2/hook/'
+
+        # 要发送的JSON数据
+        json_data = {
+            'msg_type': 'text',
+            'content': {'text': content}
+        }
+
+        # 头信息
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # 发送POST请求，并指定数据为JSON格式以及头信息
+        response = requests.post(url, json=json_data, headers=headers)
+
+        # 检查响应状态码
+        response.raise_for_status()  # 如果响应状态码不是200，会抛出HTTPError异常
+
+        # 解析响应内容（假设服务器返回的是JSON格式）
+        response_data = response.json()
+        print(response_data)
+
+    except requests.exceptions.RequestException as e:
+        print(f"发送飞书消息请求失败: {e}")
+
+
+if __name__ == "__main__":
+    """
+    curl -X POST -H "Content-Type: application/json" -d "{\"msg_type\":\"text\",\"content\":{\"text\":\"request example\"}}" https://open.feishu.cn/open-apis/bot/v2/hook/xxxx
+    """
+    send_feishu_msg("request example")
