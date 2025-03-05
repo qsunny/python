2ff027d0583b1e5cc9a051b4bf4e0be00ae71e64
aaron.qiu <247986063@qq.com>
2025-03-05 10:18:47 +0800

    feat:anythingllm example

---
+++ yolo/predict/count-object01.py
@@ -0,0 +1,35 @@
+import cv2
+
+from ultralytics import solutions
+
+
+"""
+统计人、车示例
+"""
+
+
+def count_objects_in_region(video_path, output_video_path, model_path):
+    """Count objects in a specific region within a video."""
+    cap = cv2.VideoCapture(video_path)
+    assert cap.isOpened(), "Error reading video file"
+    w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+    video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+    region_points = [(20, 400), (1080, 400), (1080, 360), (20, 360)]
+    counter = solutions.ObjectCounter(show=True, region=region_points, model=model_path)
+
+    while cap.isOpened():
+        success, im0 = cap.read()
+        if not success:
+            print("Video frame is empty or video processing has been successfully completed.")
+            break
+        im0 = counter.count(im0)
+        video_writer.write(im0)
+
+    cap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    count_objects_in_region("C:/Users/Administrator/Videos/traffic.mp4", "C:/Users/Administrator/Videos/output_video.avi", "yolo11n.pt")
