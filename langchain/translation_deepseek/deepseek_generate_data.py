d338d5cb3e1138907d30a5793fbdd81992652caf
aaron.qiu <aaron.qiu@moyuinfo.com>
2025-07-15 16:11:10 +0800

    feat:生成数据

---
+++ langchain/translation_deepseek/deepseek_generate_data.py
@@ -0,0 +1,92 @@
+import datetime
+from typing import Optional, List
+
+import uvicorn
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import YoutubeLoader
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_experimental.synthetic_data import create_data_generation_chain
+from pydantic.v1 import BaseModel, Field
+from pytube import YouTube
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+import requests
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# os.environ['http_proxy']= 'socks5://127.0.0.1:10808'
+# os.environ['https_proxy']= 'socks5://127.0.0.1:10808'
+
+os.environ['http_proxy']= 'http://127.0.0.1:10809'
+os.environ['https_proxy']= 'http://127.0.0.1:10809'
+
+# 后续请求自动使用代理
+# response = requests.get('https://www.youtube.com')
+# print(f"当前IP: {response.text}")  # 验证代理生效
+
+# 加载环境变量
+load_dotenv()
+
+# 初始化 DeepSeek 客户端
+"""
+https://api-docs.deepseek.com/zh-cn/quick_start/parameter_settings
+按使用场景设置 temperature
+代码生成/数学解题   	0.0
+数据抽取/分析	1.0
+通用对话	1.3
+翻译	1.3
+创意类写作/诗歌创作	1.5
+"""
+deepseek = ChatOpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com/v1",  # DeepSeek API 入口
+    model="deepseek-chat",                   # 使用 DeepSeek 官方模型
+    temperature=1.3,                         # 控制创意度 (0-1)
+)
+
+# 初始化ChatOpenAI模型
+# deepseek = ChatOpenAI(
+#     model="anthropic/claude-3.7-sonnet",  # 可以在OpenRouter模型列表中选择
+#     api_key=os.getenv("OPENROUTER_API_KEY"),
+#     base_url="https://openrouter.ai/api/v1",
+#     temperature=0.5,
+#     max_tokens=1024
+# )
+
+# 创建链
+chain = create_data_generation_chain(llm=deepseek)
+
+#生成数据
+# 给于一些关键词，随机生成一句话
+# result = chain(
+#     {
+#         "fields":['蓝色','黄色'],
+#         "preferences":{}
+#     }
+# )
+
+result = chain.invoke(
+    {
+        "fields":['蓝色', '黄色'],
+        "preferences":{"style": "七言律诗"}
+    }
+)
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
